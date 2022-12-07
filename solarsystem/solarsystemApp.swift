6e5346e184f740af382ad4276fe82bd8f9d13fb1
hadeel <hadeel.alturaier@hotmail.com>
2022-12-07 18:18:25 +0300

    Initial Commit

---
+++ solarsystem/solarsystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  solarsystemApp.swift
+//  solarsystem
+//
+//  Created by Hadeel Alturaier on 13/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct solarsystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
